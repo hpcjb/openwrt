51da07eff0bf6d5e1310ce46b02c6a80f6cbc619
Robert Marko <robimarko@gmail.com>
2018-06-18 17:56:40 +0200

    Update generic-ubnt.mk

--- target/linux/ath79/image/generic-ubnt.mk
+++ target/linux/ath79/image/generic-ubnt.mk
@@ -109,7 +109,7 @@ TARGET_DEVICES += ubnt-unifiac-lite
 define Device/ubnt_toughswitch-5-poe
   $(Device/ubnt-sw)
   DEVICE_TITLE := Ubiquiti Networks Toughswitch 5 POE
-  DEVICE_PACKAGES := -wpad-mini
+  DEVICE_PACKAGES := -wpad-mini -kmod-ath9k
   SUPPORTED_DEVICES += ubnt,toughswitch-5-poe toughswitch-5-poe
 endef
 TARGET_DEVICES += ubnt_toughswitch-5-poe
